a44bb1feb7c20ed6c311de2be69060c9fdf7bc9f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-03-24 14:50:21 -0700

    Creando un formulario

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -18,42 +18,82 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-8 mx-auto">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">email</th>
-                            <th scope="col">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($users as $user)
-
-                        <tr>
-                            <td>{{ $user->id}}</td>
-                            <td>{{ $user->name}}</td>
-                            <td>{{ $user->email}}</td>
-                            <td>
-                                <form action="{{ route('user.destroy', $user) }}" method="POST">
-                                    @method('DELETE')
+                <div class="card">
+                    <div class="card-header">
+                        <form action="{{ route('user.store') }}" method="POST">
+                            <div class="form-row">
+                                <div class="col">
+                                <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name') }}">
+                                </div>
+                                <div class="col">
+                                    <input type="email" name="email" class="form-control" placeholder="Correo Electronico" value="{{ old('email') }}">
+                                </div>
+                                <div class="col">
+                                    <input type="password" name="password" class="form-control" placeholder="Contraseña" value="{{ old('password') }}">
+                                </div>
+                                <div class="col-auto">
                                     @csrf
-                                    <input 
-                                        type="submit" 
-                                        value="Eliminar" 
-                                        class="btn btn-sm btn-danger" 
-                                        onclick="return confirm('Desea eliminar...??')"
-                                    >
-                                </form>
-                            </td>
-                        </tr>
-                            
-                        @endforeach
-                    </tbody>
-                </table>
+                                    <button type="submit" class="btn btn-outline-primary">Guardar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    @if ($errors->any())
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>Error!</strong> 
+                            <ul class="list-group ">
+                                @foreach ($errors->all() as $error)
+                                    <li class="list-group-item bg-transparent border-0 m-0 p-0">{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                                            
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                             </button>
+                        </div>
+                        
+                    @endif
+
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">email</th>
+                                <th scope="col">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+
+                            <tr>
+                                <td>{{ $user->id}}</td>
+                                <td>{{ $user->name}}</td>
+                                <td>{{ $user->email}}</td>
+                                <td>
+                                    <form action="{{ route('user.destroy', $user) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input 
+                                            type="submit" 
+                                            value="Eliminar" 
+                                            class="btn btn-sm btn-outline-danger" 
+                                            onclick="return confirm('Desea eliminar...??')"
+                                        >
+                                    </form>
+                                </td>
+                            </tr>
+                                
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </html>
